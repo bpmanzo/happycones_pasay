bbdacb259fa2a1a34c60cb78f9b8b3c0d8ca0887
bpmanzo <bpmanzo@student.apc.edu.ph>
2021-01-11 20:52:31 +0800

    initial

---
+++ ecommerce/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2020 Happy Cones by <a href="https://bigscoop.com.ph/">Big Scoop</a></strong>
+</footer>
---
+++ ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 Happy Cones by <a href="https://bigscoop.com.ph/">Big Scoop</a></strong>
+    </div>
+</footer>
